fcf792670891599b82244842671fd6d22e00d698
Ryuichi Suzuki <untitled.tex@gmail.com>
2021-03-15 22:26:55 +0900

    Add: function mul

--- bitflyer_py.py
+++ bitflyer_py.py
@@ -4,6 +4,10 @@ def add(x: int, y: int):
 def div(x: int, y: int):
     return (int)(x / y)
 
+def mul(x: int, y: int):
+    return x * y
+
 if __name__ == '__main__':
     print(add(12, 13))
     print(div(12, 13))
+    print(mul(12, 13))
