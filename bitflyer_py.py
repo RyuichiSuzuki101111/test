2210ed1933f870a6f9fde9994e0cbd1d26c88983
Ryuichi Suzuki <untitled.tex@gmail.com>
2021-03-15 22:34:03 +0900

    add: sub function

--- bitflyer_py.py
+++ bitflyer_py.py
@@ -4,6 +4,10 @@ def add(x: int, y: int):
 def div(x: int, y: int):
     return (int)(x / y)
 
+def sub(x: int, y: int):
+    return x - y
+
 if __name__ == '__main__':
     print(add(12, 13))
     print(div(12, 13))
+    print(sub(12, 13))
